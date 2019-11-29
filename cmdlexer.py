dcbbdbb232c1074461134f43f0b88dc035bc4884
florian.weingartshofer <florian.weingartshofer@gmail.com>
2019-11-29 11:16:00 +0100

    Init Commit

---
+++ cmdlexer.py
@@ -0,0 +1,18 @@
+from pygments.lexer import RegexLexer
+from pygments.token import *
+
+
+class CommandLexer(RegexLexer):
+    name = 'cmd'
+    aliases = ['cmd']
+    filenames = ['*.sh']
+
+    tokens = {
+        'root': [
+            (r'#.*$', Comment.Multiline),
+            (r'\".+\"', Text),
+            (r'^[A-Za-z0-9]+\s+',Keyword),
+            (r'--?[^ \n]+\s+', Generic.Inserted),
+            (r'[^ \n]+\s+', Generic.Deleted),
+        ]
+    }
