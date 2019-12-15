7addbeca4ef15939b8788f29514362715be35ca9
florian.weingartshofer <florian.weingartshofer@gmail.com>
2019-12-15 16:58:50 +0100

    Updated Lexer

--- cmdlexer.py
+++ cmdlexer.py
@@ -9,11 +9,15 @@ class CommandLexer(RegexLexer):
 
     tokens = {
         'root': [
-            (r'#.*$', Comment.Multiline),
-            (r'\".+\"', Text),
-            (r'^[ ]{0,}[A-Za-z0-9]+\s+',Keyword),
-            (r'\|[ ]{0,}[A-Za-z0-9]+\s+', Keyword),
-            (r'--?[^ \n]+\s+', Generic.Inserted),
-            (r'[^ \n]+\s+', Generic.Deleted),
+            (r'#.*$', Comment),				# A Comment
+            (r'[\>\<=~\+\-]', Operator),
+            (r'\".+\"', Literal),			# A String
+            (r'\'.+\'', Literal),			# A String
+            (r'^[ ]{0,}[A-Za-z0-9]+',Keyword),		# A Command
+            (r'\|[ ]{0,}[A-Za-z0-9]+', Keyword),	# Piping in commands
+            (r'\$[A-Za-z0-9_]+', Name.Variable),	# Variables like $USER
+            (r'--?[^ \n]+', Generic.Inserted),		# Options in a command
+            (r'[^ \n]+', Generic.Deleted),     		# Arguments
+            (r'\s', Name),				# Ignore Whitespace
         ]
     }
